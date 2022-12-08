5898ea65ea74271bbdd211bd55733feba31fe50f
Tyler Collins <tk11br@sharcnet.ca>
2022-12-08 13:25:00 -0500

    fix refactor on day 1

--- problem-1/refactored.py
+++ problem-1/refactored.py
@@ -1,6 +1,6 @@
 with open('large.txt') as f:
     block_list = []
     for block in f.read().split('\n\n'):
-        block_list.append(sum([int(i) for i in block.split('\n')]))
+        block_list.append(sum(int(i) for i in block.split('\n')))
 block_list.sort()
 print('Part 1: ', block_list[-1], 'Part 2: ', sum(block_list[-3:]))
