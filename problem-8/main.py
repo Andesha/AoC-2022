4ed72a8288eea0e60d237914765a8e6afb25b8ec
Tyler Collins <tk11br@sharcnet.ca>
2022-12-08 11:57:44 -0500

    add p2 for day 8
    feels good now at least

--- problem-8/main.py
+++ problem-8/main.py
@@ -1,15 +1,29 @@
+from functools import reduce
+
 with open('large.txt') as f:
-    content = [list(map(int,line)) for line in f.read().splitlines()]
+    trees = [list(map(int,line)) for line in f.read().splitlines()]
+trees_range = range(len(trees))
 
-visible_count = 0
-for y in range(1, len(content) - 1):
-    for x in range(1, len(content[y]) - 1):
-        max_height = content[y][x]
-        if (max(content[y][:x]) < max_height or
-            max(content[y][x+1:]) < max_height or
-            max(row[x] for i, row in enumerate(content) if i < y) < max_height or 
-            max(row[x] for i, row in enumerate(content) if i > y) < max_height 
-           ):
+def tree_caster(j, i, y, x):
+    count = 1
+    while True:
+        ty, tx = y + (j * count), x + (i * count) # step
+        if (ty not in trees_range) or (tx not in trees_range):
+            return True, count - 1 # -1 since you never actually "go" there
+        elif (trees[ty][tx] >= trees[y][x]):
+            return False, count # done, you went x
+        count += 1
+
+visible_count, best_tree = 0, 0
+for y in trees_range:
+    for x in trees_range:
+        ub, uc = tree_caster(1, 0, y, x)  # up
+        db, dc = tree_caster(-1, 0, y, x) # down
+        lb, lc = tree_caster(0, -1, y, x) # left
+        rb, rc = tree_caster(0, 1, y, x)  # right
+
+        if any([ub, db, rb, lb]):
             visible_count += 1
+        best_tree = max(reduce(lambda x, y: x * y, [uc, dc, rc, lc]), best_tree)
 
-print('Part 1: ', (len(content)*4) - 4 + visible_count)
+print('Part 1: ', visible_count, 'Part 2: ', best_tree)
